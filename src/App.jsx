f19b4bb67a77b843218ac887c0dd0d0afeb546cd
Stefan Heine <stheine@arcor.de>
2018-02-06 21:29:48 +0100

    page label

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div>My App</div>
+        <div>Jalousie</div>
         <table>
           <tbody>
             {this.renderRows()}
